9efb3b383ecfef5d17d2e87e919fdab2bb9b8b71
rustam <yussupovrustam>
2020-11-21 11:33:46 +0600

    city add && city edit

--- public/admin/dev/src/views/citys/Citys.vue
+++ public/admin/dev/src/views/citys/Citys.vue
@@ -1,8 +1,12 @@
 <template>
     <b-card-group>
         <b-card header="Список городов">
-            <b-button size="sm" class="mar-top" :hidden="!button_hidden" variant="success" @click="showModal('add')"><i class="fa fa-plus"></i> Добавить</b-button>
-            <b-button size="sm" class="mar-top" :hidden="button_hidden" variant="warning" @click="showModal('edit')"><i class="fa fa-edit"></i> Редактировать</b-button>
+            <b-button size="sm" class="mar-top" :hidden="!button_hidden" variant="success" @click="showModal('add')"><i
+                class="fa fa-plus"></i> Добавить
+            </b-button>
+            <b-button size="sm" class="mar-top" :hidden="button_hidden" variant="warning" @click="showModal('edit')"><i
+                class="fa fa-edit"></i> Редактировать
+            </b-button>
 
             <b-table
                 selectable
@@ -22,7 +26,6 @@
                 :bordered="true"
                 :items="filtered"
                 :fields="fields">
-
             </b-table>
 
             <b-row>
@@ -49,7 +52,7 @@
                 </b-col>
 
                 <b-col sm="3">
-                    <div class="text-right">{{recordsDisplayed}}</div>
+                    <div class="text-right">{{ recordsDisplayed }}</div>
                 </b-col>
             </b-row>
         </b-card>
@@ -58,88 +61,101 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex';
-  import memberForm from './City'
+import {mapGetters} from 'vuex';
+import memberForm from './City'
 
-  export default {
+export default {
     name: 'Citys',
-    components: {memberForm},
-    data: () => {
-      return {
-        action: '',
-        member_selected: undefined,
-        button_hidden: true,
 
-        currentPage: 1,
-        totalRows: 0,
-        perPage: 10,
-        fields: [
-          {key: 'name', label: 'Наименование города', sortable: true},
-        ],
-        filters: {
-          name: '',
+    components: {memberForm},
+
+    data: () => {
+        return {
+            action: '',
+            member_selected: undefined,
+            button_hidden: true,
+
+            currentPage: 1,
+            totalRows: 0,
+            perPage: 10,
+            fields: [
+                {key: 'name', label: 'Наименование города', sortable: true},
+            ],
+            filters: {
+                name: '',
+            }
         }
-      }
     },
     methods: {
-      showModal(action) {
-        this.action = action
-        this.$bvModal.show('member-form')
-      },
-      rowSelected(items) {
-        let member_selected = undefined
-        if (items.length > 0) {
-          member_selected = {...items[0]}
-        }
+        showModal(action) {
+            this.action = action
+            this.$bvModal.show('member-form')
+        },
 
-        this.button_hidden = items.length === 0
-        this.member_selected = member_selected
-      },
-      clearFilter() {
-        for (let key in this.filters) {
-          this.filters[key] = ''
+        rowSelected(items) {
+            let member_selected = undefined
+
+            if (items.length > 0) {
+                member_selected = {...items[0]}
+            }
+
+            this.button_hidden = items.length === 0
+            this.member_selected = member_selected
+        },
+
+        clearFilter() {
+            for (let key in this.filters) {
+                this.filters[key] = ''
+            }
+        },
+
+        refresh() {
+            this.$store.dispatch('citys/getCitysDataInStorage').catch(error => {
+                console.log(error)
+            })
         }
-      },
-      refresh() {
-        this.$store.dispatch('citys/getCitysDataInStorage').catch(error => { console.log(error) })
-      }
     },
     computed: {
-      ...mapGetters({
-        items: 'citys/getCitys'
-      }),
-      filtered () {
-        const filtered = this.items.filter(item => {
-          return Object.keys(this.filters).every(key =>
-            String(item[key]).includes(this.filters[key]))
-        })
+        ...mapGetters({
+            items: 'citys/getCitys'
+        }),
 
-        this.totalRows = filtered.length
-        this.currentPage = 1
+        filtered() {
+            const filtered = this.items.filter(item => {
+                return Object.keys(this.filters).every(key =>
+                    String(item[key]).includes(this.filters[key]))
+            })
 
-        return filtered.length > 0 ? filtered : []
-      },
-      recordsDisplayed() {
-        let how_much = this.perPage * this.currentPage - this.perPage + 1
-        let how_many = this.perPage * this.currentPage
-        let str = how_much + ' - ' + how_many
+            this.totalRows = filtered.length
+            this.currentPage = 1
 
-        if (this.totalRows === 0) {
-          str = '0'
-        } else if (how_many < this.totalRows || how_many > this.totalRows) {
-          str = how_much + ' - ' + this.totalRows
+            return filtered.length > 0 ? filtered : []
+        },
+
+        recordsDisplayed() {
+            let how_much = this.perPage * this.currentPage - this.perPage + 1
+            let how_many = this.perPage * this.currentPage
+            let str = how_much + ' - ' + how_many
+
+            if (this.totalRows === 0) {
+                str = '0'
+            } else if (how_many < this.totalRows || how_many > this.totalRows) {
+                str = how_much + ' - ' + this.totalRows
+            }
+
+            return 'Отображено ' + str + ' из ' + this.totalRows
         }
-
-        return 'Отображено '+ str + ' из ' + this.totalRows
-      }
     },
+
     mounted() {
-      this.$store.dispatch('citys/getCitysDataInStorage').catch(error => { console.log(error) })
+        this.$store.dispatch('citys/getCitysDataInStorage').catch(error => {
+            console.log(error)
+        })
     }
-  }
+}
 </script>
 <style scoped>
-    .mar-top {
-        margin-bottom: 1em;
-    }
+.mar-top {
+    margin-bottom: 1em;
+}
 </style>
