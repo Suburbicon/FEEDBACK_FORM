591ced9fd0f8435934157f83dac8128f933926fa
icespr1n <lost8672@gmail.com>
2020-11-23 15:10:43 +0600

    Fix 0.0.4

--- public/admin/dev/src/storage/modules/citys.js
+++ public/admin/dev/src/storage/modules/citys.js
@@ -10,7 +10,7 @@ export default {
     actions: {
         getCitysDataInStorage({commit}) {
             return new Promise((resolve, reject) => {
-                axios.post('/city/get/')
+                axios.post('/city/get')
                 .then(response => {
                     commit('setCityData', response.data)
                     resolve(response.data)
