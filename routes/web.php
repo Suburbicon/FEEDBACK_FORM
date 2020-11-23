31a64bdb259843bdd594cc89b81ad7f64a4cebc7
icespr1n <lost8672@gmail.com>
2020-11-23 14:47:13 +0600

    Fix 0.0.3

--- routes/web.php
+++ routes/web.php
@@ -25,42 +25,42 @@ Route::group(
 //    Route::get('/', 'DashboardController@index')->name('dashboard');
 
 
-        Route::post('/orders/get/', 'Orders\OrderController@getData')->name('ajax');
-        Route::post('/orders/add/', 'Orders\OrderController@Add');
-        Route::post('/orders/edit/', 'Orders\OrderController@Edit');
-        Route::post('/orders/addcompany/', 'Orders\OrderController@AddCompany');
+        Route::post('/orders/get', 'Orders\OrderController@getData')->name('ajax');
+        Route::post('/orders/add', 'Orders\OrderController@Add');
+        Route::post('/orders/edit', 'Orders\OrderController@Edit');
+        Route::post('/orders/addcompany', 'Orders\OrderController@AddCompany');
 
-        Route::post('/companies/get/', 'Companies\CompanyController@getData');
-        Route::post('/companies/add/', 'Companies\CompanyController@Add');
-        Route::post('/companies/edit/', 'Companies\CompanyController@Edit');
-        Route::post('/companies/searchbyid/', 'Companies\CompanyController@SearchByMemberId');
-        Route::post('/companies/getactivitieslist/', 'Companies\ActivitiesController@getList');
-        Route::post('/companies/gettypeslist/', 'Companies\TypesController@getList');
+        Route::post('/companies/get', 'Companies\CompanyController@getData');
+        Route::post('/companies/add', 'Companies\CompanyController@Add');
+        Route::post('/companies/edit', 'Companies\CompanyController@Edit');
+        Route::post('/companies/searchbyid', 'Companies\CompanyController@SearchByMemberId');
+        Route::post('/companies/getactivitieslist', 'Companies\ActivitiesController@getList');
+        Route::post('/companies/gettypeslist', 'Companies\TypesController@getList');
 
-        Route::post('/members/get/', 'Members\MemberController@getData');
-        Route::post('/members/add/', 'Members\MemberController@Add');
-        Route::post('/members/edit/', 'Members\MemberController@Edit');
-        Route::post('/members/search/', 'Members\MemberController@Search');
-        Route::post('/members/connecttocompany/', 'Members\MemberController@setRelationshipToCompany');
+        Route::post('/members/get', 'Members\MemberController@getData');
+        Route::post('/members/add', 'Members\MemberController@Add');
+        Route::post('/members/edit', 'Members\MemberController@Edit');
+        Route::post('/members/search', 'Members\MemberController@Search');
+        Route::post('/members/connecttocompany', 'Members\MemberController@setRelationshipToCompany');
 
-        Route::any('/city/get/', 'Citys\CityController@getData');
-        Route::post('/city/add/', 'Citys\CityController@Add');
-        Route::post('/city/edit/', 'Citys\CityController@Edit');
+        Route::any('/city/get', 'Citys\CityController@getData');
+        Route::post('/city/add', 'Citys\CityController@Add');
+        Route::post('/city/edit', 'Citys\CityController@Edit');
 
-        Route::post('/appeals/get/', 'Appeals\AppealController@getData');
+        Route::post('/appeals/get', 'Appeals\AppealController@getData');
 //    Route::post('/city/add/', 'Citys\CityController@Add');
 //    Route::post('/city/edit/', 'Citys\CityController@Edit');
 
-        Route::post('/departments/get/', 'Departments\DepartmentController@getData');
-        Route::post('/departments/add/', 'Departments\DepartmentController@Add');
-        Route::post('/departments/edit/', 'Departments\DepartmentController@Edit');
+        Route::post('/departments/get', 'Departments\DepartmentController@getData');
+        Route::post('/departments/add', 'Departments\DepartmentController@Add');
+        Route::post('/departments/edit', 'Departments\DepartmentController@Edit');
 
-        Route::post('/sectors/get/', 'Sectors\SectorController@getData');
-        Route::post('/sectors/add/', 'Sectors\SectorController@Add');
-        Route::post('/sectors/edit/', 'Sectors\SectorController@Edit');
-        Route::post('/sectors/get_qr/', 'Sectors\SectorController@AddQR');
+        Route::post('/sectors/get', 'Sectors\SectorController@getData');
+        Route::post('/sectors/add', 'Sectors\SectorController@Add');
+        Route::post('/sectors/edit', 'Sectors\SectorController@Edit');
+        Route::post('/sectors/get_qr', 'Sectors\SectorController@AddQR');
 
-        Route::post('/config/get/', 'ConfigController@getAllConfig');
+        Route::post('/config/get', 'ConfigController@getAllConfig');
     }
 );
 
